63d4ba45d6047aa707e3ef81c6e333f4a1b530b2
jvelesquen <justovelesquen@gmail.com>
2022-02-04 15:34:04 -0300

    nuevos hechos

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -106,10 +106,10 @@ function esPar(num) {
   // Devuelve "true" si "num" es par
   // De lo contrario, devuelve "false"
   // Tu código:
-  if (Number% 2 == 0){
+  if (num % 2 == 0){
     return true
   }
-  return false
+ {return false}
 }
 function esImpar(num) {
   // Devuelve "true" si "num" es impar
@@ -122,19 +122,19 @@ function elevarAlCuadrado(num) {
   // Devuelve el valor de "num" elevado al cuadrado
   // ojo: No es raiz cuadrada!
   // Tu código:
-  
+  return num ** 2
 }
 
 function elevarAlCubo(num) {
   // Devuelve el valor de "num" elevado al cubo
   // Tu código:
-  
+  return num ** 3
 }
 
 function elevar(num, exponent) {
   // Devuelve el valor de "num" elevado al exponente dado en "exponent"
   // Tu código:
-  
+  return num ** exponent
 }
 
 function redondearNumero(num) {
