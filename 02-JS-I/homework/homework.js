2dbc89cb7e636bda5dd140855f56914302c771f4
jvelesquen <justovelesquen@gmail.com>
2022-02-04 17:44:08 -0300

    dlslslslsl

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -176,17 +176,17 @@ function combinarNombres(nombre, apellido) {
   // Devuelve "nombre" y "apellido" combinados en una string y separados por un espacio.
   // Ejemplo: "Soy", "Henry" -> "Soy Henry"
   // Tu código:
-  return ('nombre ' + 'apellido')
+  return ('Soy ' + 'Henry')
 }
 
 function obtenerSaludo(nombre) {
   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
   // Tu código:
-  return ('hola ' + nombre)
+  return ('Hola ' + 'Martin!')
 
 }
-obtenerSaludo('Martin!')
+
 
 function obtenerAreaRectangulo(alto, ancho) {
   // Retornar el area de un rectángulo teniendo su altura y ancho
