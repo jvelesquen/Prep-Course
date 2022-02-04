412fc0f0b3653a25fb9bec478ed9c7aafe2cb193
jvelesquen <justovelesquen@gmail.com>
2022-02-04 17:47:03 -0300

    jfjgkfk

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -170,20 +170,21 @@ function agregarSimboloExclamacion(str) {
   // Agrega un símbolo de exclamación al final de la string "str" y devuelve una nueva string
   // Ejemplo: "hello world" pasaría a ser "hello world!"
   // Tu código:
+  return ('hello world' + '!')
 }
 
 function combinarNombres(nombre, apellido) {
   // Devuelve "nombre" y "apellido" combinados en una string y separados por un espacio.
   // Ejemplo: "Soy", "Henry" -> "Soy Henry"
   // Tu código:
-  return ('hello ' + 'world')
+  return ('Soy ' + 'Henry')
 }
 
 function obtenerSaludo(nombre) {
   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
   // Tu código:
-  return ('Hola ' + 'Antonio!')
+  return ('Hola ' + 'Martin!')
 
 }
 
